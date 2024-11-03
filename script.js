516f7d1325ad17d956065a0caa231482835781a0
Anthony Zavala <anzavala@ucsd.edu>
2024-11-02 18:47:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-document.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const links = document.querySelectorAll(".sidebar-link");
+// Script to highlight the active section title in the sidebar
+window.addEventListener('scroll', () => {
+  const sections = document.querySelectorAll('section');
+  const sidebarLinks = document.querySelectorAll('.sidebar-link');
 
-    let current = "";
+  let current = '';
 
-    sections.forEach(section => {
-        const sectionTop = section.getBoundingClientRect().top;
-        if (sectionTop <= 0 && sectionTop + section.clientHeight > 0) {
-            current = section.getAttribute("id");
-        }
-    });
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute('id');
+    }
+  });
 
-    links.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${current}`) {
-            link.classList.add("active");
-        }
-    });
+  sidebarLinks.forEach((link) => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
 });
